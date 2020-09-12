b4bca5ac15354602b10a9cc3753cb6f9addb91ba
Lance Ewing <lance.ewing@gmail.com>
2020-09-13 00:33:48 +0100

    Updated pack-zip.sh to use index.html in the zip file.

--- pack-zip.sh
+++ pack-zip.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 ./pack-min.sh
-rm zips/*.zip
-tools/kzip.exe //b512 zips/min_kzip.zip min.html
-tools/advzip.exe -a -4 -i 10000 zips/min_advzip.zip min.html
+cd zips
+cp ../min.html index.html
+rm *.zip
+../tools/kzip.exe //b512 min_kzip.zip index.html
+../tools/advzip.exe -a -4 -i 10000 min_advzip.zip index.html
+cd ..
